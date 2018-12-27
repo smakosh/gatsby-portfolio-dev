cec3ab2aa739b9fbdd96c5b5bb7a3ef3c958b397
Ismail Ghallou <ismai23l@hotmail.com>
2018-12-27 18:50:33 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,11 +62,11 @@ module.exports = {
 		{
 			resolve: 'gatsby-plugin-manifest',
 			options: {
-				name: 'gatsby-starter-default',
+				name: config.defaultTitle,
 				short_name: 'starter',
 				start_url: '/',
-				background_color: '#663399',
-				theme_color: '#663399',
+				background_color: config.backgroundColor,
+				theme_color: config.themeColor,
 				display: 'minimal-ui',
 				icon: './static/favicon/favicon-512.png',
 			},
