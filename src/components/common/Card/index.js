1f9b25c956f106bef899b3ee64b094c97cb2a618
Ben <wolfnun011@gmail.com>
2020-10-14 20:43:54 +0900

    update: styling for card

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -5,3 +5,9 @@ export const Card = styled.div`
   background: ${({ theme }) => (theme === 'light' ? '#fff' : '#181717')};
   height: 100%;
 `;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
