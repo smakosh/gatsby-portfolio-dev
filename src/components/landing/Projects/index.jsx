752fc3281b6e09c4d0fa46360e0b9072b5e66d60
Ben <wolfnun011@gmail.com>
2020-10-14 20:49:49 +0900

    fix: double colon to single colon

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { ThemeContext } from 'providers/ThemeProvider';
-import { Container, Card, TitleWrap } from "components/common";
+import { Container, Card, TitleWrap } from 'components/common';
 import Star from 'components/common/Icons/Star';
 import Fork from 'components/common/Icons/Fork';
 import { Wrapper, Grid, Item, Content, Stats, Languages } from "./styles";
